33c316db0d5e5ad644f4087172dfbf1f030c2541
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-01 17:44:09 +0100

    feat: add accordion list on main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
+import Languages from "../../data/Languages";
 import Accordion from "./Accordion";
 
 export default function Main() {
   return (
     <main>
       <section className="accordions">
-        <Accordion />
+        {Languages.map((language) => {
+          return (
+            <Accordion
+              key={language.id}
+              title={language.title}
+              description={language.description}
+            />
+          );
+        })}
       </section>
     </main>
   );
